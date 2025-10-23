423e7ed36b6a22e018449dabaa122e364bf77cb6
azri1228 <azrirasydan28@gmail.com>
2025-10-23 23:01:05 +0700

    nambah file html

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan ID-NYS
+const tombolsapa = document.getElementById('sapaButton');
+// 2. tambahan 'event listener'untuk aksi 'click'
+tombolsapa.addEventListener('click', function() {
+    // 3. jalankan kode ini ketika tombol di link
+    alert('Halo! terima kasih sudah berkunjung!');
+});
